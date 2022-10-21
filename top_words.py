a9a3ef2ac2f12c251493356ca14b6d5323f66907
MaksymFedorov22 <114014008+MaksymFedorov22@users.noreply.github.com>
2022-10-21 08:58:24 +0300

    Add files via upload

---
+++ top_words.py
@@ -0,0 +1,23 @@
+﻿while True:
+    mText = input("Введіть текст: ").split()
+
+    z = input("Введіть що зробити A B C exit: ")
+
+    if z.lower() == "a":
+
+        print("А is not ready yet")
+
+    elif z.lower() == "b":
+        mText.sort()
+        for i in range(len(mText)):
+            print(mText[i])
+    elif z.lower() == "c":
+        mText = set(mText)
+        mText = list(mText)
+        mText.sort(reverse = True, key = len)
+        for i in range(5):
+            print(mText[i])
+    elif z.lower() == "exit":
+        break
+    else:
+        print("Such a task does not exist")
